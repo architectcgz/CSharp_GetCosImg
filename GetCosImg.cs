7ec014e5c40a5da387e17256d9d44a4fedf33212
architectcgz <119087313+architectcgz@users.noreply.github.com>
2023-11-19 19:00:36 +0800

    修改创建文件的算法
    修改了创建文件的算法，适配性更高，且将每页的所有图片单独放到一个文件夹中，如第10页cos图\【cos正片】《剑网3九周年》秦风秀姐\【cos正片】《剑网3九周年》秦风秀姐cosplay欣赏cos正片-第1张.jpg'

--- GetCosImg.cs
+++ GetCosImg.cs
@@ -1,8 +1,4 @@
 ﻿using HtmlAgilityPack;
-using System.Diagnostics;
-using System.Threading;
-
-
 namespace Program
 {
     class Program
@@ -22,13 +18,21 @@ namespace Program
             int pageNumLow = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("请输入要获取的图片范围上限(上限<=下限<=14)");
             int pageNumHigh = Convert.ToInt32(Console.ReadLine());
-
-            var (titleList, imgURLList) = await getCosImg.getImgURLWithPageNum(pageNumLow,pageNumHigh);
-            var dict = await getCosImg.getEachImgDetail(imgURLList);
-            for(int i =0;i<imgURLList.Count;i++)
+            for(int i = pageNumLow;i<= pageNumHigh; i++)
             {
-                await getCosImg.DownLoadAll(dict,titleList);
+                Directory.CreateDirectory($"第{i}页cos图");
+                var (titleList, imgURLList) = await getCosImg.getImgURLWithPageNum(i);
+                Console.WriteLine($"第{i}页有{imgURLList.Count}组图片，开始获取页面中全部图片的url信息");
+                var dict = await getCosImg.getEachImgDetail(imgURLList);
+                for (int j = 0; j < imgURLList.Count; j++)
+                {
+                    await getCosImg.DownLoadAll(dict, titleList, i);
+                }
+                Console.WriteLine($"第{i}页cos图下载完成，请查看文件夹");
+                Console.WriteLine("防止请求速度过快，休眠1秒");
+                await Task.Delay(1000);
             }
+           
             Console.WriteLine("已经全部下载完成，请查看文件夹");
             Console.WriteLine("点击任意键退出...");
             Console.ReadKey();
@@ -36,25 +40,20 @@ namespace Program
     }
     class GetCosImg
     {
-        public async Task<(List<string>, List<string>)> getImgURLWithPageNum(int pageNumLow,int pageNumHigh)
+        public async Task<(List<string>, List<string>)> getImgURLWithPageNum(int pageNum)
         {
-            if (pageNumLow <1||pageNumHigh>14)
+            if (pageNum <1||pageNum>14)
             {
-                Console.WriteLine("页数范围下限或上限输入错误,1<=页数<=14！");
+                Console.WriteLine("页数范围输入错误,1<=页数<=14！");
                 return (new List<string>(), new List<string>());
             }
             string url;
             var imgURLList = new List<string>();
             var imgTitleList = new List<string>();
-            
-            for(int i = pageNumLow; i <= pageNumHigh; i++)
-            {
-                url = $"https://www.ciyuanjie.cn/cosplay/page_{i}.html";
-                var result = await getImgURLAndTitleOnePage(url);
-                imgURLList.AddRange(result.Item1);
-                imgTitleList.AddRange(result.Item2);
-            }
-            
+            url = $"https://www.ciyuanjie.cn/cosplay/page_{pageNum}.html";
+            var result = await getImgURLAndTitleOnePage(url);
+            imgURLList.AddRange(result.Item1);
+            imgTitleList.AddRange(result.Item2);
             return (imgURLList, imgTitleList);
         }
         public async Task<(List<string>,List<string>)> getImgURLAndTitleOnePage(string url)
@@ -72,7 +71,8 @@ namespace Program
                 var titleNodes = document.DocumentNode.SelectNodes("//div[@class='posr-tit']");
                 foreach ( var titleNode in titleNodes )
                 {
-                    titleList.Add(titleNode.InnerText);
+                    //去掉标题字符串中的空格
+                    titleList.Add(titleNode.InnerText.Replace(" ",""));
                     //Console.WriteLine(title);
                 }
                 var nodes = document.DocumentNode.SelectNodes("//div[@class='kzpost-data']/a[1]");
@@ -109,8 +109,8 @@ namespace Program
                 //src是图片url
                 var src = node.GetAttributeValue("src", "");
                 Console.WriteLine(src);
-                // alt也就是文件名
-                var alt = node.GetAttributeValue("alt", "");
+                // alt也就是文件名 注意去除title字符串中的空格
+                var alt = node.GetAttributeValue("alt", "").Replace(" ","");
                 Console.WriteLine(alt);
                 if (!dict.ContainsKey(alt))
                     dict.Add(alt, src);
@@ -126,7 +126,6 @@ namespace Program
         public async Task<Dictionary<string,string>> getEachImgDetail(List<string>imgUrlList)
         {
             string detailedUrl = "https://www.ciyuanjie.cn/";
-            Console.WriteLine($"本页有{imgUrlList.Count}组图片");
             var tasks = new List<Task<Dictionary<string, string>>>();
             for (int i = 0; i < imgUrlList.Count; i++)
             {
@@ -169,6 +168,7 @@ namespace Program
                 if (!response.IsSuccessStatusCode)
                     return false;
                 var stream = await response.Content.ReadAsStreamAsync();
+
                 using var fileStream = System.IO.File.Create(storagePath);
                 if (stream == null)
                     return false;
@@ -195,40 +195,50 @@ namespace Program
             return new string(title.Where(c => !invalidChars.Contains(c)).ToArray());
         }
         /// <summary>
-        /// 基于Task的并行处理下载所有图片
+        /// 基于Task的并行处理下载所有图片 下载第pageNum页cos图
         /// 如果图片数量过大，会占用非常大的内存，所以考虑使用线程池替代Task并行处理
         /// </summary>
         /// <param name="dict"></param>
         /// <param name="titleList"></param>
+        /// <param name="pageNum">第pageNum页cos图</param>
         /// <returns></returns>
-        public async Task DownLoadAll(Dictionary<string, string> dict, List<string> titleList)
+        public async Task DownLoadAll(Dictionary<string, string> dict, List<string> titleList,int pageNum)
         {
+            // 创建一个SemaphoreSlim对象，初始化为24，表示最多允许24个线程同时运行
+            var semaphore = new SemaphoreSlim(24);
 
-            //创建目录
-            for (int i = 0; i < titleList.Count; i++)
-            {
-                string path = $"{titleList[i]}";
-                path = RemoveInvalidChars(path);
-                Console.WriteLine($"正在创建目录{path}");
-
-                if (!Directory.Exists(path))
-                    Directory.CreateDirectory(path);
-            }
             var tasks = new List<Task>();
             foreach (var key in dict.Keys)
             {
-                //格式为《宿命回响》DESTINY cosplay欣赏@杜杜Dolly_ cosplay-第一张   -第1张去掉-
-                int index = key.IndexOf("cosplay",key.IndexOf("cosplay")+1);
-                string path = RemoveInvalidChars(key.Substring(0,index-1)) + "\\" + RemoveInvalidChars(key) + ".jpg";
+                //找到最后一个图片名中的cos
+                //【cos正片】好帅的安哥《凹凸世界》安迷修cos欣赏cosplay-第1张.jpg
+                //使用【cos正片】好帅的安哥《凹凸世界》安迷修cos欣赏  作为文件夹名
+                int folderIndex = key.LastIndexOf("cos");
+                string folderPath = RemoveInvalidChars(key.Substring(0, folderIndex));
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory($"第{pageNum}页cos图\\{folderPath}");
+                }
+
+                string filePath = $"第{pageNum}页cos图\\{folderPath}\\{RemoveInvalidChars(key)}.jpg";
                 var task = Task.Run(async () =>
                 {
-                    Console.WriteLine($"正在将文件保存到{path}");
-                    var result = await DownloadFromURL(dict[key], path);
-                    if (!result)
+                    try
                     {
-                        Console.WriteLine($"{key}下载失败，等待重新下载");
-                        return;
+                        Console.WriteLine($"正在将文件保存到{filePath}");
+                        var result = await DownloadFromURL(dict[key], filePath);
+                        if (!result)
+                        {
+                            Console.WriteLine($"{key}下载失败，等待重新下载");
+                            return;
+                        }
                     }
+                    finally
+                    {
+                        // 完成任务后，释放semaphore
+                        semaphore.Release();
+                    }
+                    
                 });
                 tasks.Add(task);
             }
